94e39f07ec0a972c538f1528e1615b023c41f4c7
katreenapo <katja.djadechko1@gmail.com>
2021-01-10 18:50:23 +0200

    Завдання Angular - TypeScript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'HomeWorks';
-  todos = [{
+  todos :Todo [] =[{
     name: 'Task 1',
     isDone: true,
+    id:1,
       }]
     
       addTask() {
         this.todos.push({
         name: 'Task 2',
-        isDone: false,},
+        isDone: false,
+      id:2},
         {
           name: 'Task 3',
-          isDone: true,})
+          isDone: true,
+        id: 3})
       }
 }
